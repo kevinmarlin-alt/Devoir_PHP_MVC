f06554a3b68419ec35f9d082cae565bb14d5c1a3
Kevin Marlin <kevin.marlin@gmail.com>
2026-06-15 14:20:10 +0200

    feat(layouts): add a header layout

---
+++ app/Views/Layouts/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Touche pas au klaxon - <?= $title ?></title>
+</head>
+<body>
+    <header>
+        <h1>Touche pas au klaxon</h1>
+    </header>
