d7ed549e95052ceb93a5c6f0739c49a45bbce2c8
Kevin Marlin <kevin.marlin@gmail.com>
2026-06-17 09:49:29 +0200

    feat(read): add icons CTA on travels table

--- app/Views/Layouts/index.php
+++ app/Views/Layouts/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Site intranet pour la gestion des trajets de déplacement entre agences.">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/styles/style.css">
     <title>Touche pas au klaxon - <?= $title ?></title>
 </head>
 <body>
