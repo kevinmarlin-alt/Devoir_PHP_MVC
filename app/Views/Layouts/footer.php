b18efb6ad5a73f974fce182b931c2948b293cde1
Kevin Marlin <kevin.marlin@gmail.com>
2026-06-29 10:22:04 +0200

    refact(footer): change the name of application

--- app/Views/Layouts/footer.php
+++ app/Views/Layouts/footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center mt-4">
-    <p class="mb-0"><time datetime="2026">2026</time> - CEF - MVC PHP</p>
+  <p class="mb-0">© <time datetime="2026">2026</time> Touche pas au klaxon — Tous droits réservés.</p>
 </footer>
